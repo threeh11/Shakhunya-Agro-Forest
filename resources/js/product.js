ce093aa28f6fb720ab404d3caf586c6fff833ce6
threeh11 <lemass338@mail.ru>
2023-01-14 22:55:29 +0300

    осталось сделать js

--- resources/js/product.js
+++ resources/js/product.js
@@ -1,9 +1,146 @@
-function getDescription() {
-    $('#description').addClass('block');
+function remoteOther(nameMain) {
+    switch (nameMain) {
+        case 'desc':
+        {
+            $('#questionBlock')
+                .removeClass('block')
+                .addClass('hidden');
+            $('#questionMenu')
+                .addClass('border-[#E7E7E7]')
+                .removeClass('border-[#FFED4E]');
+            $('#reviewsBlock')
+                .removeClass('block')
+                .addClass('hidden');
+            $('#reviewsMenu')
+                .addClass('border-[#E7E7E7]')
+                .removeClass('border-[#FFED4E]');
+            $('#contactsBlock')
+                .removeClass('block')
+                .addClass('hidden');
+            $('#contactsMenu')
+                .addClass('border-[#E7E7E7]')
+                .removeClass('border-[#FFED4E]');
+        } break;
+        case 'review':
+        {
+            $('#questionBlock')
+                .removeClass('block')
+                .addClass('hidden');
+            $('#questionMenu')
+                .addClass('border-[#E7E7E7]')
+                .removeClass('border-[#FFED4E]');
+            $('#contactsBlock')
+                .removeClass('block')
+                .addClass('hidden');
+            $('#contactsMenu')
+                .addClass('border-[#E7E7E7]')
+                .removeClass('border-[#FFED4E]');
+            $('#descriptionBlock')
+                .removeClass('block')
+                .addClass('hidden');
+            $('#descriptionMenu')
+                .addClass('border-[#E7E7E7]')
+                .removeClass('border-[#FFED4E]');
+        } break;
+        case 'question':
+        {
+            $('#contactsBlock')
+                .removeClass('block')
+                .addClass('hidden');
+            $('#contactsMenu')
+                .addClass('border-[#E7E7E7]')
+                .removeClass('border-[#FFED4E]');
+            $('#descriptionBlock')
+                .removeClass('block')
+                .addClass('hidden');
+            $('#descriptionMenu')
+                .addClass('border-[#E7E7E7]')
+                .removeClass('border-[#FFED4E]');
+            $('#reviewsBlock')
+                .removeClass('block')
+                .addClass('hidden');
+            $('#reviewsMenu')
+                .addClass('border-[#E7E7E7]')
+                .removeClass('border-[#FFED4E]');
+        } break;
+        case 'contacts':
+        {
+            $('#descriptionBlock')
+                .removeClass('block')
+                .addClass('hidden');
+            $('#descriptionMenu')
+                .addClass('border-[#E7E7E7]')
+                .removeClass('border-[#FFED4E]');
+            $('#reviewsBlock')
+                .removeClass('block')
+                .addClass('hidden');
+            $('#reviewsMenu')
+                .addClass('border-[#E7E7E7]')
+                .removeClass('border-[#FFED4E]');
+            $('#contactsBlock')
+                .removeClass('block')
+                .addClass('hidden');
+            $('#contactsMenu')
+                .addClass('border-[#E7E7E7]')
+                .removeClass('border-[#FFED4E]');
+        }
+    }
 }
+function getDescription() {
+    $('#descriptionMenu')
+        .addClass('border-[#FFED4E]')
+        .removeClass('border-[#E7E7E7]');
+
+    $('#descriptionBlock')
+        .removeClass('hidden')
+        .addClass('block');
+    remoteOther('desc');
+}
+
+function getReviews() {
+    $('#reviewsMenu')
+        .addClass('border-[#FFED4E]')
+        .removeClass('border-[#E7E7E7]');
+
+    $('#reviewsBlock')
+        .removeClass('hidden')
+        .addClass('block');
+    remoteOther('review');
+}
+
+function getQuestions() {
+    $('#questionMenu')
+        .addClass('border-[#FFED4E]')
+        .removeClass('border-[#E7E7E7]');
+
+    $('#questionBlock')
+        .removeClass('hidden')
+        .addClass('block');
+    remoteOther('desc');
+}
+
+function getContacts() {
+    $('#contactsMenu')
+        .addClass('border-[#FFED4E]')
+        .removeClass('border-[#E7E7E7]');
+
+    $('#contactsBlock')
+        .removeClass('hidden')
+        .addClass('block');
+    remoteOther('review');
+}
+
 $(document).ready(function() {
-    $('#buttonBuy').click(function (){
-        alert(11);
+    $('#descriptionMenu').click(function (){
+        getDescription();
+    });
+    $('#reviewsMenu').click(function (){
+        getReviews();
+    });
+    $('#questionMenu').click(function (){
+        getQuestions();
+    });
+    $('#contactsMenu').click(function (){
+        getContacts();
     });
 });
-
--- resources/views/productPage.blade.php
+++ resources/views/productPage.blade.php
@@ -6,12 +6,20 @@
     <x-content @class(['mt-10'])>
         <x-breadcrumb :product=$product />
         <div class="flex flex-row w-full h-[592px]">
-            <div class="w-[50%]">
+            <div class="flex flex-row gap-2">
                 @foreach($listImages as $image)
-                    <img class="w-[472px] h-[454px]" src="{{ asset($image) }}" alt="{{ $product->name }}" id="{{ 'image' . $loop->iteration }}" main="true">
+                    @if (1  === $loop->iteration)
+                        <div class="flex flex-col gap-2">
+                            <img class="w-[110px] h-[110px]" src="{{ asset($image) }}" alt="{{ $product->name }}" id="{{ 'image' . $loop->iteration }}">
+                    @elseif (2 === $loop->iteration)
+                            <img class="w-[110px] h-[110px]" src="{{ asset($image) }}" alt="{{ $product->name }}" id="{{ 'image' . $loop->iteration }}">
+                        </div>
+                    @elseif (3 === $loop->iteration)
+                        <img class="w-[472px] h-[454px]" src="{{ asset($image) }}" alt="{{ $product->name }}" id="{{ 'image' . $loop->iteration }}">
+                    @endif
                 @endforeach
             </div>
-            <div class="w-[50%] flex flex-col">
+            <div class="pl-10 flex flex-col">
                 <h1 class="text-[#333333] font-['Manrope'] text-3xl  leading-10 font-normal pb-7">
                     {{ $product->name }}
                 </h1>
@@ -22,6 +30,32 @@
                     Купить
                 </button>
             </div>
+        </div>
+        <div class="flex flex-row justify-between">
+            <x-h3menu id="descriptionMenu" @class(['border-[#FFED4E]'])>
+                Описание
+            </x-h3menu>
+            <x-h3menu id="reviewsMenu">
+                Отзывы
+            </x-h3menu>
+            <x-h3menu id="questionMenu">
+                Вопросы
+            </x-h3menu>
+            <x-h3menu id="contactsMenu">
+                Контакты
+            </x-h3menu>
+        </div>
+        <div class="hidden pt-5 text-[#333333] font-['Manrope'] text-base leading-6 font-normal" id="descriptionBlock">
+            {{ $product->description }}
+        </div>
+        <div class="hidden pt-5 text-[#333333] font-['Manrope'] text-base leading-6 font-normal" id="reviewsBlock">
+            {{ $product->description }}
+        </div>
+        <div class="hidden pt-5 text-[#333333] font-['Manrope'] text-base leading-6 font-normal" id="questionBlock">
+            {{ $product->description }}
+        </div>
+        <div class="hidden pt-5 text-[#333333] font-['Manrope'] text-base leading-6 font-normal" id="contactsBlock">
+            {{ $product->description }}
         </div>
     </x-content>
 
