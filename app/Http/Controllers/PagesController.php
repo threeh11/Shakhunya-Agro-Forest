2b9c513cb2867ac70d70390b4b6d8598e80f3963
threeh11 <lemass338@mail.ru>
2023-01-14 23:14:25 +0300

    база для егыча

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\GetAllProductsAction;
 use App\Actions\GetProductAction;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
@@ -9,16 +10,21 @@ class PagesController extends Controller
 {
     private GetProductAction $getProduct;
 
+    private GetAllProductsAction $getAllProducts;
+
     public function __construct(
         GetProductAction $getProduct,
+        GetAllProductsAction $getAllProducts,
     )
     {
         $this->getProduct = $getProduct;
+        $this->getAllProducts = $getAllProducts;
     }
 
     public function index(): View
     {
-        return view('index');
+        $dataView = $this->getAllProducts->handle();
+        return view('index', $dataView);
     }
 
     public function product(int $idProduct): View
