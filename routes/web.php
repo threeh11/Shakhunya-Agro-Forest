ab786c7c973d9ec7a74c26d30b9ab52121f4c9c1
threeh11 <lemass338@mail.ru>
2023-01-14 13:08:12 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(PagesController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
 });
