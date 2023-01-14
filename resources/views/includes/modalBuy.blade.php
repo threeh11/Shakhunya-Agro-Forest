03b7e751b9a683e4ef09a0d806df9302d94f3a0f
user <sashaotmachov@gmail.com>
2023-01-15 00:18:28 +0300

    добавил модальное окно

--- resources/views/includes/modalBuy.blade.php
+++ resources/views/includes/modalBuy.blade.php
@@ -1,5 +1,35 @@
-<div class="block ">
+<div class="drop-shadow-2xl pt-5 w-[500px] h-[714px] left-[23%] top-[23%] fixed block rounded-xl bg-[#FFFFFF] ">
+    <p class="mx-2.5 color-[#333333] leading-8 text-2xl font-normal font-[Manrope]">
+        Оптовый заказ производителю
+    </p>
+    <p class="mx-2.5 my-3 color-[#333333] font-normal font-[Manrope] font-size: 0,875rem leading-5">
+        Пожалуйста укажите данные о вашей организации, 
+        интересующий объем продукции, а также другую 
+        информацию, которую вы посчитаете важной 
+    </p>
     <form action="">
-
+        <div class="flex flex-col border-none">
+            <p class="my-2.5 mx-2.5 border-solid color-[#333333] font-[Manrope] text-base font-bold">
+                Ваше имя
+            </p>
+            <input class="mx-2.5 w-[90%] h-11 border rounded border-solid border-[#CCCCCC]" type="text"> 
+            <p class="my-2.5 mx-2.5 color-[#333333] font-[Manrope] text-base font-bold">
+                Телефон
+            </p>
+            <input class="mx-2.5 w-[90%] h-11 border rounded border-solid border-[#CCCCCC]" type="text">
+            <p class="my-2.5 mx-2.5 border-solid color-[#333333] font-[Manrope] text-base font-bold">
+                Электронная почта
+            </p>
+            <input class="mx-2.5 w-[90%] h-11 border rounded border-solid border-[#CCCCCC]">
+            <p class="my-2.5 mx-2.5 border-solid color-[#333333] font-[Manrope] text-base font-bold">
+                Текст заказа
+            </p>
+            <input class="mx-2.5 w-[90%] h-32 border rounded border-solid border-[#CCCCCC]" type="text">
+            <div class="mx-2.5 my-2.5 w-[60%] h-11 float-left text-center rounded bg-yellow-300">
+                <p class="mx-2.5 my-2.5 w-[90%] color-[#333333] font-[Manrope] text-base font-normal">
+                    Отправить
+                </p>
+            </div>
+        </div>
     </form>
 </div>
