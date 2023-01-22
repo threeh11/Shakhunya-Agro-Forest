ce4a57707fb8ba98c5d31fcb2a9002b266226894
threeh11 <lemass338@mail.ru>
2023-01-22 11:29:23 +0300

    Сделал открытие фоток и затемнение фона

--- resources/views/productPage.blade.php
+++ resources/views/productPage.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.base')
 
 @include('includes.modalBuy')
-<div class="hidden" id="placePhoto">
-    <img id="showImage" alt="photo" src="" width="700px" height="600px"/>
+<div class="bg-black bg-opacity-50 h-screen w-full hidden" id="placePhoto">
+    <div id="photo">
+        <img id="showImage" alt="photo" src="" width="700px" height="600px"/>
+    </div>
 </div>
 
 @section('content')
